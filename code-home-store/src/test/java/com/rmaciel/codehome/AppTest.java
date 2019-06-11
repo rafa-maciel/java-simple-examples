be316198709d2d2bb23b7b9dacc4a40b31f3a408
rafamaciel <rafael.macielf@gmail.com>
2019-06-11 10:09:26 -0300

    create project to practice spring mvc

---
+++ code-home-store/src/test/java/com/rmaciel/codehome/AppTest.java
@@ -0,0 +1,20 @@
+package com.rmaciel.codehome;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
